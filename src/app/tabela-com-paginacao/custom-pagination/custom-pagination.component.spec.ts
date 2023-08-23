da7db414be67a39b0f9752838707a011570f7538
Jefferzom <jeffsilvati@hotmail.com>
2023-08-23 20:10:21 -0300

    wip test 2

--- src/app/tabela-com-paginacao/custom-pagination/custom-pagination.component.spec.ts
+++ src/app/tabela-com-paginacao/custom-pagination/custom-pagination.component.spec.ts
@@ -1,18 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomPaginationComponent } from './custom-pagination.component';
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef, Component, ElementRef, NO_ERRORS_SCHEMA, ViewChild } from '@angular/core';
 
+@Component({
+  template: `
+    <app-custom-pagination
+    [pageSizeOptions]="[5, 10, 15, 20, 100]" 
+    [pageSize]="5" 
+    [length]="10"
+    ></app-custom-pagination>
+  `
+})
+class ParentPaginatorComponent {
+  
+}
 describe('CustomPaginationComponent', () => {
   let component: CustomPaginationComponent;
   let fixture: ComponentFixture<CustomPaginationComponent>;
+  let parentPaginatorFixture: ComponentFixture<ParentPaginatorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CustomPaginationComponent ]
+      declarations: [ CustomPaginationComponent, ParentPaginatorComponent ],
+      imports: [MatPaginatorModule],
+      providers: [MatPaginatorIntl, ChangeDetectorRef],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(CustomPaginationComponent);
+    parentPaginatorFixture = TestBed.createComponent(ParentPaginatorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,4 +39,8 @@ describe('CustomPaginationComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
 });
--- src/app/tabela-com-paginacao/custom-pagination/custom-pagination.component.ts
+++ src/app/tabela-com-paginacao/custom-pagination/custom-pagination.component.ts
@@ -1,5 +1,9 @@
-import { ChangeDetectorRef, Component} from '@angular/core';
-import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorIntl, PageEvent } from '@angular/material/paginator';
+
+interface CustomPageEvent extends PageEvent {
+  pageEvent: string;
+}
 
 @Component({
   selector: 'app-custom-pagination',
@@ -7,6 +11,8 @@ import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
   styleUrls: ['./custom-pagination.component.scss']
 })
 export class CustomPaginationComponent extends MatPaginator {
+  @ViewChild('matSelect', {read: ElementRef}) matSelect!: ElementRef
+
   constructor(intl: MatPaginatorIntl, changeDetectorRef: ChangeDetectorRef) {
     super(intl, changeDetectorRef);
   }
@@ -16,45 +22,50 @@ export class CustomPaginationComponent extends MatPaginator {
   }
 
   createEllipse() {
-    let current = this.pageIndex + 1;
-
-    const max = Math.ceil(this.length/this.pageSize);
-
-    const items: any = [];
-
-    const addPage = (page: any, isActive: any) => {
-      items.push({ page, active: isActive });
-    };
-
-    const addEllipse = () => {
-      items.push({ page: '...', active: false });
-    };
-
-    if (max <= 5) {
-      for (let i = 1; i <= max; i++) {
-        addPage(i, i === current);
+    console.log(this.length)
+    if (this.length) {
+      let current = this.pageIndex + 1;
+  
+      const max = Math.ceil(this.length/this.pageSize);
+  
+      const items: any = [];
+  
+      const addPage = (page: any, isActive: any) => {
+        items.push({ page, active: isActive });
+      };
+  
+      const addEllipse = () => {
+        items.push({ page: '...', active: false });
+      };
+  
+      if (max <= 5) {
+        for (let i = 1; i <= max; i++) {
+          addPage(i, i === current);
+        }
+      } else {
+        addPage(1, current === 1);
+  
+        if (current > 3) {
+          addEllipse();
+        }
+  
+        let r1 = Math.max(current - 1, 2);
+        let r2 = Math.min(current + 1, max - 1);
+  
+        for (let i = r1; i <= r2; i++) {
+          addPage(i, i === current);
+        }
+  
+        if (current < max - 2) {
+          addEllipse();
+        }
+  
+        addPage(max, max === current);
       }
+      return items;
     } else {
-      addPage(1, current === 1);
-
-      if (current > 2) {
-        addEllipse();
-      }
-
-      let r1 = Math.max(current - 1, 2);
-      let r2 = Math.min(current + 1, max - 1);
-
-      for (let i = r1; i <= r2; i++) {
-        addPage(i, i === current);
-      }
-
-      if (current < max - 1) {
-        addEllipse();
-      }
-
-      addPage(max, max === current);
+      return [];
     }
-    return items;
   }
 
   hasPage(pageNumber: number):boolean {
@@ -71,12 +82,32 @@ export class CustomPaginationComponent extends MatPaginator {
     this._myEmitPageEvent(previousPageIndex);
   }
 
-  private _myEmitPageEvent(previousPageIndex: number) {
-    this.page.emit({
+  private _myEmitPageEvent(previousPageIndex?: number, action?: any) {
+    const evAction = action
+    const customPageEvent: CustomPageEvent = {
       previousPageIndex,
       pageIndex: this.pageIndex,
       pageSize: this.pageSize,
-      length: this.length
-    });
+      length: this.length,
+      pageEvent: evAction
+    };
+  
+    this.page.emit(customPageEvent);
+  }
+
+  getEventArrow(ev?: string) {
+    console.log(ev)
+    this._myEmitPageEvent(undefined, ev)
+  }
+
+  @HostListener('document:click', ['$event.target'])
+  onClickOutSide(target: any) {
+    const matSelectElement: HTMLElement = this.matSelect.nativeElement;
+
+    if(matSelectElement.contains(target)) {
+      matSelectElement.classList.add('move-arrow')
+    } else {
+      matSelectElement.classList.remove('move-arrow')
+    }
   }
 }
